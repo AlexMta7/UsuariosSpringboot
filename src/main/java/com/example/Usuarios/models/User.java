eccb1265d90584dbfc4489d453ebf9bdf5094a15
Alex <alexrobertomata@hotmail.com>
2022-10-18 14:59:03 -0600

    Entidad usuario con errores - arreglar

---
+++ src/main/java/com/example/Usuarios/models/User.java
@@ -0,0 +1,25 @@
+package com.example.Usuarios.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "user")
+@Getter @Setter
+@ToString @EqualsAndHashCode
+public class User {
+
+    private Long id;
+    private String name;
+    private String lastName;
+    private String email;
+    private String name;
+    private String name;
+
+    
+}
